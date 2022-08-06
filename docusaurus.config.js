5d3a87bb60937d548ffba99223860bd87446ed92
lavren1974 <lavren1974@gmail.com>
2022-08-06 22:30:51 +0700

    20220806-1830

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,6 +57,9 @@ const config = {
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
+          //blogTitle: 'Блог Docusaurus!',
+          //blogDescription: 'Блог на сайте Docusaurus!',
+          //postsPerPage: 'ALL',
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
