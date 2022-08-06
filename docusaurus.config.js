1a5c70164304997c5af0183db5fb343d26c4873b
lavren1974 <lavren1974@gmail.com>
2022-08-06 14:08:24 +0700

    20220806-1008

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,7 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+//const npm2yarn = require('@docusaurus/remark-plugin-npm2yarn');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
